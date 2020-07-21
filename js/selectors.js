a9a18bba918f6bc40e146c19228f517def2ebbee
glorious-cyborg-8787 <ajn252@gmail.com>
2020-07-21 15:19:41 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector(){
+  
+}
+
+function lastImageSelector(){
+  
+}
+
+function ninjaBabySelector(){
+  $('#baby-ninja')
+}
+
+function divSelector(){
+  
+}
+
+function firstListItem(){
+  
+}
